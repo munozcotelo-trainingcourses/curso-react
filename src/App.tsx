28789d3dacf7189391d9e48ea4189340cf0f24a1
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2021-12-14 08:49:12 +0100

    Ejercicio 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import React from "react";
-import logo  from "./logo.svg";
+// import logo  from "./logo.svg";
 
 import './App.css';
 
 interface IAppComponentProps {
 }
 
-const AppComponent : ( props : IAppComponentProps ) => React.ReactElement = ( props : IAppComponentProps ) => {
-  return (
-      <h1>Hola mundo soy Alvaro</h1>
-  );
+// const AppComponent : ( props : IAppComponentProps ) => React.ReactElement = ( props : IAppComponentProps ) => {
+//   return (
+//       <h1>Hola mundo soy Alvaro</h1>
+//   );
+// }
+
+class AppComponent extends React.Component<IAppComponentProps, Record<string, unknown>> {
+
+    public componentDidMount() : void {
+        console.info( "Estoy en componentDidMount" );
+    }
+
+    public componentDidUpdate() : void {
+        console.info( "Estoy en componentDidUpdate" );
+    }
+
+    public componentWillUnmount() : void {
+        console.info( "Estoy en componentWillUnmount" );
+    }
+
+    public render(): React.ReactNode {
+
+        // const me: AppComponent = this;
+
+        console.info( "Estoy en render" );
+
+        return (
+            <h1>Hola mundo soy Alvaro (componente de clase)</h1>
+        );
+
+    }
+
 }
 
 export default AppComponent;
